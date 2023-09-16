02d6c95984b7b55496320c6903bccdfda8cd8540
Stefano Pisano <s.pisano@techgap.it>
2023-09-16 23:16:58 +0200

    chore: removes commented code

--- script/script.js
+++ script/script.js
@@ -1,13 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-	// document.getElementById('dark-mode-toggle').addEventListener('click', () => {
-	// 	const htmlTag = document.getElementsByTagName("body")[0];
-	//
-	//
-	// 	htmlTag.style.filter = !htmlTag.style.filter || htmlTag.style.filter === 'none'
-	// 		? 'invert(100%)'
-	// 		: 'none';
-	// })
-
 
 	document.getElementById('dark-mode-toggle').addEventListener("change", () => {
 		document.body.classList.toggle("dark")
